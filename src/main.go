7eaee018f8b762a3cf1409bfbe99aa7aff29d8a8
Jeremy White <jcwhite414@gmail.com>
2025-11-03 07:22:47 -0600

    updated repl with parsing

--- src/main.go
+++ src/main.go
@@ -7,15 +7,14 @@ import (
 	"os/user"
 )
 
-
 func main() {
-user, err := user.Current()
-if err != nil {
-	panic(err)
-}
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
 
-fmt.Printf("Hwllo %s! This isthe Monkey proggramming language!\n", user.Username)
-fmt.Printf("Feel free to type in commands\n")
-repl.Start(os.Stdin, os.Stdout)
+	fmt.Printf("Hello %s! This is the Monkey proggramming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 
 }
--- src/repl/repl.go
+++ src/repl/repl.go
@@ -5,32 +5,61 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
 const PROMP = ">> "
 
-// REPL stand for REad Eval Print Loop... 
+// REPL stand for REad Eval Print Loop...
 // A javascript console in a web browser is a REPL:
-// It allows input (Read), 
-// that input is evaluated into code (Eval), 
+// It allows input (Read),
+// that input is evaluated into code (Eval),
 // the output of the evaluated code is printed (Print),
 // and then the program switches back to Read mode (Loop)
-func Start (in io.Reader, out io.Writer) {
+func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	for {
 		fmt.Fprintf(out, PROMP)
 		scanned := scanner.Scan()
-		if (!scanned) {
+		if !scanned {
 			return
 		}
 
 		line := scanner.Text()
-
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
